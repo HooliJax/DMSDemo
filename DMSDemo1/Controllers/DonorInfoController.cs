a07917866459c6e5cb737fcf179aa220e3bac7c6
Tejas <Tejas@LAPTOP-6HIKU7P4>
2016-10-04 19:49:45 -0400

    Updated mock table with fields requested by Hope Haven and added "Export to Excel" functionality

--- DMSDemo1/Controllers/DonorInfoController.cs
+++ DMSDemo1/Controllers/DonorInfoController.cs
@@ -11,13 +11,13 @@ namespace DMSDemo1.Controllers
     {
         private readonly List <DonorInfo>donors = new List<DonorInfo>()
     {
-        new DonorInfo { Id = 1, Name = "Julio Avellaneda", Email = "julito_gtu@hotmail.com" },
-        new DonorInfo { Id = 2, Name = "Juan Torres", Email = "jtorres@hotmail.com" },
-        new DonorInfo { Id = 3, Name = "Oscar Camacho", Email = "oscar@hotmail.com" },
-        new DonorInfo { Id = 4, Name = "Gina Urrego", Email = "ginna@hotmail.com" },
-        new DonorInfo { Id = 5, Name = "Nathalia Ramirez", Email = "natha@hotmail.com" },
-        new DonorInfo { Id = 6, Name = "Raul Rodriguez", Email = "rodriguez.raul@hotmail.com" },
-        new DonorInfo { Id = 7, Name = "Johana Espitia", Email = "johana_espitia@hotmail.com" }
+        new DonorInfo { Id = 1, FirstName = "Julio", Middle = "A", LastName = "Avellaneda", Title = "Mr.", Suffix = "", CompanyName = "Google", Address = "", City = "Jacksonville", State = "FL", Zip = 33225, Email = "julito_gtu@hotmail.com", Phone = "", Birthday = "11/12/1991", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 2, FirstName = "Bob", Middle = "B", LastName = "Smith", Title = "Mr.", Suffix = "", CompanyName = "Ajax", Address = "", City = "Jacksonville", State = "FL", Zip = 33285, Email = "bob@hotmail.com", Phone = "", Birthday = "08/12/1991", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 3, FirstName = "Tom", Middle = "C", LastName = "Ford", Title = "Mr.", Suffix = "", CompanyName = "FSCJ", Address = "", City = "Jacksonville", State = "FL", Zip = 33223, Email = "tom@hotmail.com", Phone = "", Birthday = "06/06/1982", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 4, FirstName = "Lupe", Middle = "D", LastName = "Fiasco", Title = "Mr.", Suffix = "", CompanyName = "FNF", Address = "", City = "Jacksonville", State = "FL", Zip = 30025, Email = "lupe@hotmail.com", Phone = "", Birthday = "08/08/1986", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 5, FirstName = "Kendrick", Middle = "E", LastName = "Lamar", Title = "Mr.", Suffix = "", CompanyName = "TDE", Address = "", City = "Jacksonville", State = "FL", Zip = 30205, Email = "kdot@hotmail.com", Phone = "", Birthday = "04/24/1975", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 6, FirstName = "Warren", Middle = "F", LastName = "Buffett", Title = "Mr.", Suffix = "", CompanyName = "", Address = "", City = "Jacksonville", State = "FL", Zip = 31115, Email = "cashmoney@hotmail.com", Phone = "", Birthday = "01/22/1978", Gender = "Male", Relationship = "N/A", Identity = "" },
+        new DonorInfo { Id = 7, FirstName = "Karthik", Middle = "G", LastName = "Umapathy", Title = "Mr.", Suffix = "", CompanyName = "UNF", Address = "", City = "Jacksonville", State = "FL", Zip = 31235, Email = "seniorproj@hotmail.com", Phone = "", Birthday = "02/10/1988", Gender = "Male", Relationship = "N/A", Identity = "" },
     };
 
         public ActionResult Index()
--- DMSDemo1/Models/DonorInfo.cs
+++ DMSDemo1/Models/DonorInfo.cs
@@ -8,7 +8,21 @@ namespace DMSDemo1.Models
     public class DonorInfo
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Middle { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Suffix { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Relationship { get; set; }
+        public string Identity { get; set; }
     }
 }
